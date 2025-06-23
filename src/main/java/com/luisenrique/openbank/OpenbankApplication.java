a176a093665e2a0c058711ea6d09f90542c4bdfc
LUIS <luisenriquejuanf@gmail.com>
2025-06-23 12:14:57 +0200

    Initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/com/luisenrique/openbank/OpenbankApplication.java
@@ -0,0 +1,13 @@
+package com.luisenrique.openbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luisenrique/openbank/OpenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisenrique.openbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
