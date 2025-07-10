b652d64967a147ed3733ffba5c7c6ba9095bc9d8
LUIS <luisenriquejuanf@gmail.com>
2025-07-10 09:15:21 +0200

    Optimized imports

--- src/main/java/com/luisenrique/openbank/account/model/BankAccount.java
+++ src/main/java/com/luisenrique/openbank/account/model/BankAccount.java
@@ -2,7 +2,10 @@ package com.luisenrique.openbank.account.model;
 
 import com.luisenrique.openbank.user.model.User;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
